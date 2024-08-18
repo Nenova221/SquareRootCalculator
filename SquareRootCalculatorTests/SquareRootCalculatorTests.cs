fbf6412ad423a6d657c523e36b392a8a49b2a763
Nenova221 <kalina.nenova@paisii.info>
2024-08-18 14:54:18 +0300

    First commit

--- SquareRootCalculatorTests/SquareRootCalculatorTests.cs
+++ SquareRootCalculatorTests/SquareRootCalculatorTests.cs
@@ -6,13 +6,13 @@ namespace SquareRootCalculatorTests
     public class SquareRootCalculatorTests
     {
         [Theory]
-        [InlineData(4, 2)]
-        [InlineData(9, 3)]
-        [InlineData(16, 4)]
+        [InlineData(12345, 111.1)]
+        [InlineData(1552, 39.4)]
+        [InlineData(753, 27.4)]
         public void Calculate_WithCalculateSquareRoot_ShouldReturnCorrectResult(double input, double expected)
         {
             double result = SquareRootCalculator.Calculate(input);
-            Assert.Equal(expected, result);
+            Assert.Equal(expected, result, 1);
         }
         [Fact]
         public void Calculate_NegativeInput_ShouldThrowArgumentOutOfRangeException()
